f4bc4a9dff6b7f0345df3e720863b8d608b2ea4c
joshua-shorterivey <joshua.shorterivey@gmail.com>
2022-12-02 09:59:45 -0600

    amended wrangle files

--- wrangle.py
+++ wrangle.py
@@ -312,13 +312,13 @@ def generate_mappings():
     region_map = {1: 'NEW ENGLAND', 2: 'MIDDLE ATLANTIC', 3: 'EAST NORTH CENTRAL', 4: 'WEST NORTH CENTRAL',
             5: 'SOUTH ATLANTIC',6: 'EAST SOUTH CENTRAL',7: 'WEST SOUTH CENTRAL', 8: 'MOUNTAIN',  9: 'PACIFIC' }
 
-    state_map = {1:'AL',2:'AK',4:'AZ',5:'AR', 6:'CA',8:'CO', 9:'CT', 10:'DE',
-                11:'DC', 12:'FL',13:'GA',15:'HI',16:'ID',17:'IL',18:'IN',
-                19:'IA',20:'KS',21:'KY',22:'LA',23:'ME',24:'MD',25:'MA',26:'MI',
-                27:'MN',28:'MS',29:'MO',30:'MT',31:'NE',32:'NV',33:'NH',34:'NJ',
-                35:'NM',36:'NY',37:'NC',38:'ND',39:'OH',40:'OK',41:'OR',42:'PA',
-                44:'RI',45:'SC',46:'SD',47:'TN',48:'TX',49:'UT',50:'VT',51:'VA',
-                53:'WA',54:'WV',55 :'WI',56:'WY'}
+    state_map = {1:'Alabama',2:'Alaska',4:'Arizona',5:'Arkansas', 6:'California',8:'Colorado', 9:'Conneticut', 10:'Deleware',
+                11:'D.C.', 12:'Florida',13:'Georgia',15:'Hawaii',16:'Idaho',17:'Illinois',18:'Indiana',
+                19:'Iowa',20:'Kansas',21:'Kentucky',22:'Lousiana',23:'Maine',24:'Maryland',25:'Massachusets',26:'Michigan',
+                27:'Minnesota',28:'Mississippi',29:'Missouri',30:'Montana',31:'Nebraska',32:'Nevada',33:'New Hampshire',34:'New Jersey',
+                35:'New Mexico',36:'New York',37:'North Carolina',38:'North Dakota',39:'Ohio',40:'Oklahoma',41:'Oregon',42:'Pennyslyvania',
+                44:'Rhode Island',45:'South Carolina',46:'South Dakota',47:'Tennesse',48:'Texas',49:'Utah',50:'Vermont',51:'Virginia',
+                53:'Washignton',54:'West Virginia',55 :'Wisconsin',56:'Wyoming'}
 
     metropolitan_map = {1:'metro', 2:'non_metro',3:'not_identified'}
 
